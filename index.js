c245086fa414a1d44c5b03457ce14487969e39d6
Kevin <kelvinngechu@gmail.com>
2022-12-13 14:37:20 +0300

    completed

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function createFareMultiplier (fare){
        return fareMultiplier;
     }
 }
+const fareDoubler=function(createFareMultiplier){
+return createFareMultiplier*2;
+}
+const fareTripler=function(createFareMultiplier){
+return createFareMultiplier*3;
+}
+
+const selectDifferentDrivers = function (drivers, otherDrivers)  {
+return otherDrivers(drivers);
+}
